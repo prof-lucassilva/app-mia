f3d12c13d99dab0a528f1b4c099c3745375fba3f
Lucas Silva <prof.lucassilvabu@gmail.com>
2024-10-26 19:44:33 -0300

    ajuste no BPM tamanho do card

--- src/pages/Data.tsx
+++ src/pages/Data.tsx
@@ -11,7 +11,7 @@ const Data: React.FC = () => {
       <h1 className="text-white mb-5 text-center text-lg sm:text-xl md:text-2xl lg:text-3xl">
         Informações de Saúde
       </h1>
-      <div className="flex flex-wrap justify-center w-full max-w-5xl overflow-y-auto">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 w-full max-w-5xl overflow-y-auto">
         {[
           { image: heartImage, title: 'BPM', value: data.heart.bpm, sig: data.heart.sig, alt: 'Frequência Cardíaca' },
           { image: flexaoImage, title: 'Flexão do Braço Esquerdo', value: data.arms.left.flex, alt: 'Flexão do Braço Esquerdo', mirror: true },
@@ -21,9 +21,9 @@ const Data: React.FC = () => {
         ].map((card, index) => (
           <motion.div
             key={index}
-            className={`bg-white shadow-md rounded-lg p-5 m-2 text-center transition-transform transform hover:scale-105 flex-1 ${
-              index === 0 ? 'w-full sm:w-2/3 lg:w-1/2' : 'max-w-xs'
-            }`} // Ajustando a largura do cartão BPM
+            className={`bg-white shadow-md rounded-lg p-5 m-2 text-center transition-transform transform hover:scale-105 ${
+              index === 0 ? 'col-span-1 sm:col-span-2 lg:col-span-3' : 'max-w-xs'
+            }`}
             initial={{ opacity: 0, x: -20 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.5, delay: index * 0.2 }}
@@ -35,7 +35,7 @@ const Data: React.FC = () => {
             />
             <p className="font-bold">{card.title}</p>
             <p className="text-2xl">{card.value}</p>
-            {index === 0 && ( // Condição para mostrar "Signal Strength" apenas no primeiro cartão
+            {index === 0 && (
               <p className="text-sm text-gray-600">
                 Signal Strength: {card.sig}
               </p>
