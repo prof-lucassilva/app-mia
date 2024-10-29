c69e6b322c863afab7be274b2edff432397a7b70
Lucas Silva <prof.lucassilvabu@gmail.com>
2024-10-29 16:59:29 -0300

    ajuste na responsividade

--- src/pages/Data2.tsx
+++ src/pages/Data2.tsx
@@ -115,82 +115,81 @@ const Data: React.FC = () => {
   }, [])
 
   return (
-    <div className="min-h-screen bg-gradient-radial from-purple-800 to-black p-4 md:p-8 overflow-x-hidden"
-    style={{
-      backgroundImage: 'radial-gradient(circle, rgba(126,27,219,0.8), rgba(0,0,0,0.8))',
-    }}>
-      {isFormVisible ? (
-        <motion.form
-          onSubmit={handleSubmit}
-          className="flex flex-col items-center mb-5 w-full max-w-xs sm:max-w-sm mx-auto"
-          initial={{ opacity: 0, y: -20 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: -20 }}
-        >
-          <h2 className="text-white text-lg mb-4">Por favor, insira suas informações:</h2>
-          <motion.input
-            type="text"
-            placeholder="Nome"
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-            className="p-2 mb-2 rounded w-full"
-            required
-          />
-          <motion.input
-            type="number"
-            placeholder="Idade"
-            value={userAge}
-            onChange={(e) => setUserAge(e.target.value)}
-            className="p-2 mb-4 rounded w-full"
-            required
-          />
-          <button type="submit" className="bg-white text-purple-500 p-2 rounded w-full">
-            Começar
-          </button>
-        </motion.form>
-      ) : (
-        <>
-          <h1 className="text-white mb-5 text-center text-lg sm:text-xl md:text-2xl lg:text-3xl w-full">
-            Informações de Saúde de {userName}, {userAge} anos
-          </h1>
-          <div className="flex flex-col lg:flex-row gap-4">
-            <div className="lg:w-1/3">
-              <motion.div
-                className={`shadow-md rounded-lg p-4 sm:p-5 mb-4 text-center transition-transform transform hover:scale-105 w-full ${getCardColor(
-                  bpm
-                )}`}
-                initial={{ opacity: 0, y: -20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5 }}
-              >
-                <img
-                  src={heartImage}
-                  alt="Frequência Cardíaca"
-                  className="w-12 h-12 sm:w-16 sm:h-16 mb-4 mx-auto"
-                />
-                <p className="font-bold">BPM</p>
-                <motion.p className="text-2xl" animate={{ scale: bpmScale }} transition={{ duration: 0.5 }}>
-                  {bpm}
-                </motion.p>
-                <p className="text-sm text-white">Signal Strength: {data.heart.sig}</p>
-
-                <div className="h-64 mt-4">
-                  <ResponsiveContainer width="100%" height="100%">
-                    <LineChart
-                      data={bpmData}
-                      margin={{ top: 5, right: 20, left: 0, bottom: 5 }}
-                    >
-                      <CartesianGrid strokeDasharray="3 3" />
-                      <XAxis dataKey="time" tick={{ fill: 'white', fontSize: 10 }} />
-                      <YAxis tick={{ fill: 'white', fontSize: 10 }} />
-                      <Tooltip contentStyle={{ backgroundColor: 'rgba(255, 255, 255, 0.8)', color: 'black' }} />
-                      <Legend wrapperStyle={{ color: 'white' }} />
-                      <Line type="monotone" dataKey="bpm" stroke="#ffffff" activeDot={{ r: 8 }} />
-                    </LineChart>
-                  </ResponsiveContainer>
-                </div>
-              </motion.div>
+    <div
+  className="min-h-screen flex flex-col bg-gradient-radial from-purple-800 to-black p-4 md:p-8 overflow-y-auto"
+  style={{
+    backgroundImage: 'radial-gradient(circle, rgba(126,27,219,0.8), rgba(0,0,0,0.8))',
+  }}
+>
+  {isFormVisible ? (
+    <motion.form
+      onSubmit={handleSubmit}
+      className="flex flex-col items-center mb-5 w-full max-w-xs sm:max-w-sm mx-auto mt-4"
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -20 }}
+    >
+      <h2 className="text-white text-lg mb-4">Por favor, insira suas informações:</h2>
+      <motion.input
+        type="text"
+        placeholder="Nome"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+        className="p-2 mb-2 rounded w-full"
+        required
+      />
+      <motion.input
+        type="number"
+        placeholder="Idade"
+        value={userAge}
+        onChange={(e) => setUserAge(e.target.value)}
+        className="p-2 mb-4 rounded w-full"
+        required
+      />
+      <button type="submit" className="bg-white text-purple-500 p-2 rounded w-full">
+        Começar
+      </button>
+    </motion.form>
+  ) : (
+    <>
+      <h1 className="text-white mb-5 text-center text-lg sm:text-xl md:text-2xl lg:text-3xl mt-30 sm:mt-10">
+        Informações de Saúde de {userName}, {userAge} anos
+      </h1>
+      <div className="flex flex-col lg:flex-row gap-4">
+        <div className="lg:w-1/3">
+          {/* Componente de BPM */}
+          <motion.div
+            className={`shadow-md rounded-lg p-4 sm:p-5 mb-4 text-center transition-transform transform hover:scale-105 w-full ${getCardColor(
+              bpm
+            )}`}
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <img
+              src={heartImage}
+              alt="Frequência Cardíaca"
+              className="w-12 h-12 sm:w-16 sm:h-16 mb-4 mx-auto"
+            />
+            <p className="font-bold">BPM</p>
+            <motion.p className="text-2xl" animate={{ scale: bpmScale }} transition={{ duration: 0.5 }}>
+              {bpm}
+            </motion.p>
+            <p className="text-sm text-white">Signal Strength: {data.heart.sig}</p>
+            <div className="h-64 mt-4">
+              <ResponsiveContainer width="100%" height="100%">
+                <LineChart data={bpmData} margin={{ top: 5, right: 20, left: 0, bottom: 5 }}>
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="time" tick={{ fill: 'white', fontSize: 10 }} />
+                  <YAxis tick={{ fill: 'white', fontSize: 10 }} />
+                  <Tooltip contentStyle={{ backgroundColor: 'rgba(255, 255, 255, 0.8)', color: 'black' }} />
+                  <Legend wrapperStyle={{ color: 'white' }} />
+                  <Line type="monotone" dataKey="bpm" stroke="#ffffff" activeDot={{ r: 8 }} />
+                </LineChart>
+              </ResponsiveContainer>
             </div>
+          </motion.div>
+        </div>
 
             <div className="lg:w-1/3">
               <div className="grid grid-cols-2 gap-4 mb-8">
@@ -229,7 +228,7 @@ const Data: React.FC = () => {
                     <ResponsiveContainer width="100%" height="100%">
                       <LineChart
                         data={armData}
-                        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+                        margin={{ top: 0, right: 30, left: 20, bottom: 5 }}
                       >
                         <CartesianGrid strokeDasharray="3 3" />
                         <XAxis dataKey="time" />
